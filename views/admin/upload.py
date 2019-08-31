4c0328cbaef0fdab213fb0b359e63fdd03097f2c
정우영 <wjd030811@dsm.hs.kr>
2019-09-01 06:04:26 +0900

    난~고칠~수없~는병에걸렸어~어어~어어~

--- views/admin/upload.py
+++ views/admin/upload.py
@@ -25,13 +25,11 @@ class Upload(Resource):
         except KeyError or TypeError:
             return {'msg': 'valueless'}, 400
 
-        buy_time = datetime.datetime.strptime(buy_time, '%Y-%m-%d')
-
         if permission != 9:
             return {'msg': 'permission_denied'}, 403
 
         query_insert_item_info = 'insert into item (uuid, title, cate, loc, point, main_img, buy_time, write_time) ' \
-                                 f"values ({uuid}, '{title}', '{cate}', '{loc}', {point}, '{main_img}', '{buy_time}', '{datetime.datetime.now()}')"
+                                 f"values ({uuid}, '{title}', '{cate}', '{loc}', {point}, '{main_img}', '{buy_time}' '{datetime.datetime.now().strftime('%Y-%m-%d')}')"
         curs.execute(query_insert_item_info)
 
         query_update_user_info = f'update user set point = point + {point} where uuid = {uuid}'
