393bbb06ad181d4cb43c24afe609f56e50442f6f
정우영 <wjd030811@dsm.hs.kr>
2019-09-01 11:04:27 +0900

    asdjfklwejklfjlawfjlawejklflawejk

--- views/service/update.py
+++ views/service/update.py
@@ -15,6 +15,17 @@ class Update(Resource):
         except KeyError or TypeError:
             return {'msg': 'valueless'}, 400
 
+        query_select_user_info = 'select point from user where uuid = %s'
+        curs.execute(query_select_user_info, uuid)
+        point = curs.fetchone()['point']
+
+        query_select_item_info = 'select point from item where item_id = %s'
+        curs.execute(query_select_item_info, item_id)
+        item_point = curs.fetchone()['point']
+
+        if point < item_point:
+            return {'msg': 'low_point'}, 403
+
         query_update_item_info = 'update item set status = %s where item_id = %s'
         curs.execute(query_update_item_info, (uuid, item_id))
         conn.commit()
